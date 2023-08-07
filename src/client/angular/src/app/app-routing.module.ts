4d356c73230fee5bc6a28b2b318979a2b57b35dd
Priya S <priya123singha@gmail.com>
2023-08-07 15:50:51 +0530

    FRONTEND: Added User Module

--- src/client/angular/src/app/app-routing.module.ts
+++ src/client/angular/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'user/register', component: UserComponent },];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
