9ce380468b7de91bfabc1cd7cdddc08b3be3e448
sulagna <sulagnachatterjee27@gmail.com>
2023-08-07 18:40:55 +0530

    First commit

--- src/client/angular/src/app/app-routing.module.ts
+++ src/client/angular/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component'
 
-const routes: Routes = [{ path: 'user/register', component: UserComponent },];
+const routes: Routes = [
+  { path: 'user/register', component: UserComponent },
+  { path: 'admin/register', component: AdminComponent },
+];
 
 
 @NgModule({
