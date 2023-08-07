7b81ac897687970b8c40aae4858d072b848c92af
Harsh Rathod <hrathore50@ymail.com>
2023-08-07 11:03:34 +0530

    BACKEND: Initial commit

---
+++ src/main/java/com/company/rll/RllApplication.java
@@ -0,0 +1,12 @@
+package com.company.rll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RllApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RllApplication.class, args);
+  }
+}
---
+++ src/test/java/com/company/rll/rll/RllApplicationTests.java
@@ -0,0 +1,12 @@
+package com.company.rll.rll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RllApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
