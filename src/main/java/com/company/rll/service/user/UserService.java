fa017487aa580e7ee51a2ac7d93031dfd4407738
Harsh Rathod <hrathore50@ymail.com>
2023-08-08 10:47:10 +0530

    BACKEND: JAVA: Fixed user login error

--- src/main/java/com/company/rll/service/user/UserService.java
+++ src/main/java/com/company/rll/service/user/UserService.java
@@ -8,27 +8,25 @@ import org.springframework.stereotype.Service;
 import com.company.rll.entity.user.UserEntity;
 import com.company.rll.repository.user.UserRepository;
 
-
 @Service
 public class UserService {
 
 	@Autowired
 	UserRepository ur;
-	
-	@Autowired
-	UserEntity u;
-	
-	public String loginvalidation(String uname,String pwd) {
-		u=ur.findByuname(uname);
-		if(u!=null) {
-			if(u.getUsername()==uname && u.getPassword()==pwd) {
+
+	public String loginvalidation(String uname, String pwd) {
+		UserEntity u = ur.findByuname(uname);
+		if (u != null) {
+			if (u.getUsername() == uname && u.getPassword() == pwd) {
 				return "login sucessfull";
+			} else {
+				return "Invalid Creds";
 			}
-			else {return "Invalid Creds";}
+		} else {
+			return "User not found";
 		}
-		else {return "User not found";}
 	}
-	
+
 	public List<UserEntity> showall() {
 		return ur.findAll();
 	}
