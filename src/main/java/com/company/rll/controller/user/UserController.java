e613d495252cc7b5d2b4dac442b091ae63d75ec6
Swaroop A V <swaroop.v@mphasis.com>
2023-08-08 19:43:05 +0530

    BACKEND:user registration merge conflict resolved

--- src/main/java/com/company/rll/controller/user/UserController.java
+++ src/main/java/com/company/rll/controller/user/UserController.java
@@ -3,17 +3,13 @@ package com.company.rll.controller.user;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-<<<<<<< Updated upstream
-=======
 import org.springframework.web.bind.annotation.RequestBody;
->>>>>>> Stashed changes
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.company.rll.entity.user.UserEntity;
