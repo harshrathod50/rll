fbb96cd2336c2085fe5fc62eede64607a5a4ef44
Sinchana Rao <sinchanarao2811@gmail.com>
2023-08-10 17:26:09 +0530

    Frontend: bus viewAll done

--- src/client/angular/src/app/user/bus/bus-list.component.ts
+++ src/client/angular/src/app/user/bus/bus-list.component.ts
@@ -1,33 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BusService } from './bus.service';
+
+export class Bus {
+  location!:string;
+  departure!:string;
+  eta!:string;
+  price!:number;
+  availability!:string;
+
+  constructor(location:string,departure:string,eta:string,price:number,availability:string){
+    this.location=location;
+    this.departure=departure;
+    this.eta=eta;
+    this.price=price;
+    this.availability=availability;
+  }
+}
 
 @Component({
   selector: 'app-bus-list',
   templateUrl: './bus-list.component.html',
   styleUrls: ['./bus-list.component.css']
 })
-export class BusListComponent {
+export class BusListComponent implements OnInit {
+
+  buses!:Bus[];
   bus?:string;
-  adminId?:string;
   constructor(private busService: BusService, private router: Router) {}
 
   ngOnInit() {
-    // this.adminId =(string) localStorage.getItem("adminId");
-    if (this.adminId == null) {
-      this.router.navigate(["/error", "login to continue"]);
-    } /*else {
-      this.busService.viewAllBus().subscribe(
-        (data: null) => {
-          this.bus = data;
-        },
-        (error: any) => {
-          this.router.navigate(["/error", "some error occured"]);
-        }
-      );
-    }*/
-  }
-
+    this.busService.viewAllBus().subscribe((response)=>{this.buses=response;},((error)=>{console.log(error);}));
+}
   removeBus(busNo: any) {
     if (confirm("are you sure you want to delete?")) {
       /*this.busService.removeBus(busNo).subscribe(
--- src/client/angular/src/app/user/bus/bus.service.ts
+++ src/client/angular/src/app/user/bus/bus.service.ts
@@ -1,13 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Bus } from './bus-list.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BusService {
+  busurl = 'http://localhost:8080/bus';
+ 
 
-  constructor() { }
-  viewAllBus(): Observable < Object > { return new Observable() } 
+  constructor(private http: HttpClient) { }
+  viewAllBus(): Observable<Bus[]> {
+     return this.http.get<Bus[]>(`${this.busurl}/viewAllbuses`);}
+
+
   removeBus(): Observable < Object > { return new Observable() } 
 }
 
