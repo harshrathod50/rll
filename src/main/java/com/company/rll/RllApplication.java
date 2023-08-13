7c68e9eb045e0f24f52d49208c1b71b7905b4bde
Harsh Rathod <hrathore50@ymail.com>
2023-08-13 13:15:02 +0530

    Configured Tests Configuration

--- src/main/java/com/company/rll/RllApplication.java
+++ src/main/java/com/company/rll/RllApplication.java
@@ -2,8 +2,9 @@ package com.company.rll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { JacksonAutoConfiguration.class })
 public class RllApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/company/rll/configuration/ApplicationConfiguration.java
+++ src/main/java/com/company/rll/configuration/ApplicationConfiguration.java
@@ -2,6 +2,7 @@ package com.company.rll.configuration;
 
 import com.company.rll.dto.admin.LocationDTO;
 import com.company.rll.entity.admin.LocationEntity;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +17,10 @@ public class ApplicationConfiguration {
       .createTypeMap(LocationDTO.class, LocationEntity.class)
       .addMappings(mapper -> mapper.skip(LocationEntity::setLocationId));
     return modelMapper;
+  }
+
+  @Bean
+  ObjectMapper objectMapper() {
+    return new ObjectMapper();
   }
 }
