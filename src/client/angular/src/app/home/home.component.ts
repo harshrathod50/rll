5832e145af3af38d4027fe2b9a7c57280f5a3953
Harsh Rathod <hrathore50@ymail.com>
2023-08-15 23:16:24 +0530

    Implemented home page

---
+++ src/client/angular/src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  host: {['class']: 'tw-grow tw-flex tw-flex-col'}
+})
+export class HomeComponent {
+
+}
