4bbad014adc01cc346dde9161e47bc7ff5fbfb0a
Harsh Rathod <hrathore50@ymail.com>
2023-09-05 13:50:26 +0530

    Implemented cloud-config-server

---
+++ emedicare-cloud-config-server/src/test/java/com/emedicare/cloudConfigServer/CloudConfigServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.emedicare.cloudConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
